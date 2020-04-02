187b43d6f0614116a725a6790aff2cd4b4ece346
Corina Bejan <54630123+corinabejan@users.noreply.github.com>
2020-04-02 21:03:21 +0200

    movieapp

--- movie/src/components/Movie.js
+++ movie/src/components/Movie.js
@@ -32,35 +32,6 @@ const Movie = function(props) {
   );
 };
 
-// class Movie extends React.Component {
-//   render() {
-//     if (!Poster || Poster === "N/A") {
-//       return null;
-//     }
 
-//     return (
-//       <div className="movie-card-container">
-//         <div className="image-container">
-//           <div
-//             className="bg-image"
-//             style={{ backgroundImage: `url(${Poster})` }}
-//           />
-//         </div>
-//         <div className="movie-info">
-//           <h2>Movie Details</h2>
-//           <div>
-//             <h1>{Title}</h1>
-//             <small>Released Date: {Released}</small>
-//           </div>
-//           <h4>Rating: {imdbRating} / 10</h4>
-//           <p>{Plot && Plot.substr(0, 350)}</p>
-//           <div className="tags-container">
-//             {Genre && Genre.split(", ").map(g => <span key={g}>{g}</span>)}
-//           </div>
-//         </div>
-//       </div>
-//     );
-//   }
-// }
 
 export default Movie;
